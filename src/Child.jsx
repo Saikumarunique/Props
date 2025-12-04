39b709d3e942cc4a780a6688bda46ad756c77447
Saikumarunique <saikumarambigarr@gmail.com>
2025-12-04 15:54:17 +0530

    cmt

--- src/Child.jsx
+++ src/Child.jsx
@@ -1,7 +1,7 @@
 import Grandchild from "./Grandchild"
 
 function Child(Props) {
-    let gchild = "arun"
+    let gchild = "aruna"
     return (
         <>
         <h1>My name is {Props.fname} and i am from {Props.fbatch} batch</h1>
